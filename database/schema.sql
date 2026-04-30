f77f875a7f00a907bf332bc6e5d44f02e91a8a99
Apostolis Falaras <apostolisfalaras@gmail.com>
2026-04-30 09:13:08 +0300

    Update database schema by adding indexes on potential data filtering fields

--- database/schema.sql
+++ database/schema.sql
@@ -1,3 +1,5 @@
+-- TABLES
+
 CREATE TABLE papers (
     -- Paper Identity Fields
     id BIGSERIAL PRIMARY KEY, -- local database unique ID
@@ -238,7 +240,7 @@ CREATE TABLE users (
 
 -- Each user might create different paper groupings called Project Folders
 -- to store series of project-related papers
-CREATE TABLE project_folders (
+CREATE TABLE user_folders (
     id SERIAL PRIMARY KEY,
     user_id INTEGER NOT NULL,
     name TEXT NOT NULL,
@@ -249,11 +251,78 @@ CREATE TABLE project_folders (
 );
 
 -- Implementation of the many-to-many paper-folder relationship
-CREATE TABLE project_folder_papers (
+CREATE TABLE user_folder_papers (
     folder_id INTEGER NOT NULL,
     paper_id BIGINT NOT NULL,
     added_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY (folder_id, paper_id),
-    FOREIGN KEY (folder_id) REFERENCES project_folders(id) ON DELETE CASCADE,
+    FOREIGN KEY (folder_id) REFERENCES user_folders(id) ON DELETE CASCADE,
     FOREIGN KEY (paper_id) REFERENCES papers(id) ON DELETE CASCADE
 );
+
+-- INDEXES (for potential filtering fields)
+
+-- Indexes for the papers table 
+CREATE INDEX idx_papers_publication_year ON papers(publication_year);
+CREATE INDEX idx_papers_publication_date ON papers(publication_date);
+CREATE INDEX idx_papers_cited_by_count ON papers(cited_by_count DESC);
+CREATE INDEX idx_papers_fwci ON papers(fwci DESC);
+CREATE INDEX idx_papers_is_open_access ON papers(is_open_access);
+CREATE INDEX idx_papers_open_access_status ON papers(open_access_status);
+CREATE INDEX idx_papers_language ON papers(language);
+CREATE INDEX idx_papers_type ON papers(paper_type);
+CREATE INDEX idx_papers_primary_source ON papers(primary_source_display_name);
+CREATE INDEX idx_papers_primary_topic ON papers(primary_topic_display_name);
+CREATE INDEX idx_papers_primary_domain ON papers(primary_domain_display_name);
+CREATE INDEX idx_papers_primary_field ON papers(primary_field_display_name);
+CREATE INDEX idx_papers_primary_subfield ON papers(primary_subfield_display_name);
+
+-- Indexes for paper_authors table
+CREATE INDEX idx_paper_authors_paper_id ON paper_authors(paper_id);
+CREATE INDEX idx_paper_authors_display_name ON paper_authors(author_display_name);
+CREATE INDEX idx_paper_authors_openalex_id ON paper_authors(author_openalex_id)
+WHERE author_openalex_id IS NOT NULL;
+
+-- Indexes for paper_author_institutions table
+CREATE INDEX idx_paper_author_institutions_author_id ON paper_author_institutions(paper_author_id);
+CREATE INDEX idx_paper_author_institutions_display_name ON paper_author_institutions(institution_display_name);
+CREATE INDEX idx_paper_author_institutions_openalex_id ON paper_author_institutions(institution_openalex_id)
+WHERE institution_openalex_id IS NOT NULL;
+CREATE INDEX idx_paper_author_institutions_country ON paper_author_institutions(country_code);
+
+-- Indexes for paper_topics table
+CREATE INDEX idx_paper_topics_paper_id ON paper_topics(paper_id);
+CREATE INDEX idx_paper_topics_name ON paper_topics(topic_display_name);
+CREATE INDEX idx_paper_topics_domain ON paper_topics(domain_display_name);
+CREATE INDEX idx_paper_topics_field ON paper_topics(field_display_name);
+CREATE INDEX idx_paper_topics_subfield ON paper_topics(subfield_display_name);
+CREATE INDEX idx_paper_topics_score ON paper_topics(score DESC);
+
+-- Indexes for paper_keywords table
+CREATE INDEX idx_paper_keywords_paper_id ON paper_keywords(paper_id);
+CREATE INDEX idx_paper_keywords_name ON paper_keywords(keyword_display_name);
+CREATE INDEX idx_paper_keyword_score ON paper_keywords(score DESC);
+
+-- Indexes for paper_locations table
+CREATE INDEX idx_paper_locations_paper_id ON paper_locations(paper_id);
+CREATE INDEX idx_paper_locations_source_name ON paper_locations(source_display_name);
+CREATE INDEX idx_paper_locations_source_type ON paper_locations(source_type);
+CREATE INDEX idx_paper_locations_is_oa ON paper_locations(is_oa);
+CREATE INDEX idx_paper_locations_is_primary ON paper_locations(is_primary);
+CREATE INDEX idx_paper_locations_is_best_oa ON paper_locations(is_best_oa);
+
+-- Indexes for paper_references table
+CREATE INDEX idx_paper_references_paper_id ON paper_references(paper_id);
+CREATE INDEX idx_paper_references_referenced_work ON paper_references(referenced_work_openalex_id);
+
+-- Indexes for paper_related table
+CREATE INDEX idx_paper_related_paper_id ON paper_related(paper_id);
+CREATE INDEX idx_paper_related_related_work ON paper_related(related_work_openalex_id);
+
+-- Indexes for counts_by_year table
+CREATE INDEX idx_paper_counts_by_year_paper_id ON paper_counts_by_year(paper_id);
+CREATE INDEX idx_paper_counts_by_year_year ON paper_counts_by_year(year);
+
+-- Indexes for user_folders & user_folder_papers table
+CREATE INDEX idx_user_folders_user_id ON user_folders(user_id);
+CREATE INDEX idx_user_folder_papers_paper_id ON user_folder_papers(paper_id);
