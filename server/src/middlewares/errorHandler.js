eda556f1bfacc229d2569060c2848921fdb0bdd4
Apostolis Falaras <apostolisfalaras@gmail.com>
2026-05-04 17:05:28 +0300

    Remove debug log message from error handler

--- server/src/middlewares/errorHandler.js
+++ server/src/middlewares/errorHandler.js
@@ -1,8 +1,7 @@
 // Global error-handling middleware defined after all routes,
 // to capture all potential errors
 export function errorHandler(err, req, res, next) {
-    console.log(err);
-
+    
     // Using the 500 error as fallback
     const statusCode = err.statusCode || 500;
     const status = err.status || "error";
