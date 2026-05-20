009c8c3aa1a3063d938e7876c6af5f9d090059df
Apostolis Falaras <apostolisfalaras@gmail.com>
2026-05-20 10:18:15 +0300

    Add return to next(); in auth to prevent the auth middlewares from running again after the route is completed

--- server/src/middlewares/authMiddleware.js
+++ server/src/middlewares/authMiddleware.js
@@ -4,23 +4,25 @@ import { AppError } from "./../utils/AppError.js";
 export function authMiddleware(req, res, next) {
     // Validating session and userId existence (if session exists)
     if (!req.session?.userId) {
-        next(new AppError("User not authenticated", 401));
+        return next(new AppError("User not authenticated", 401));
     }
 
     req.user = { 
         id: req.session.userId,
     }
-    next();
+    return next();
 }
 
 // Optional authentication for search queries that are added to the user search history
 export function optionalAuthMiddleware(req, res, next) {
-    if(!req.session?.userId)
-        next();
+    if(!req.session?.userId) {
+        req.user = null;
+        return next();
+    }
 
     req.user = {
         id: req.session.userId
     };
 
-    next();
+    return next();
 }
