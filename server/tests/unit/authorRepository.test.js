30006442f0ee3c94029ceefcda2e296cbc790612
Apostolis Falaras <apostolisfalaras@gmail.com>
2026-06-10 22:28:08 +0300

    Update author service & repository unit tests to return the top 5 papers for the author

--- server/tests/unit/authorRepository.test.js
+++ server/tests/unit/authorRepository.test.js
@@ -15,7 +15,8 @@ import {
     fetchAuthorLastKnownInstitutionsById,
     fetchAuthorTopicsById,
     fetchAuthorTopicSharesById,
-    fetchAuthorCountsByYearById } from "../../src/repositories/authorRepository.js";
+    fetchAuthorCountsByYearById,
+    fetchAuthorTop5Papers } from "../../src/repositories/authorRepository.js";
 
 
 const mockResolvedAuthor = {
@@ -146,6 +147,103 @@ const mockResolvedAuthorCountsByYear = [
         oa_works_count: 3,
         cited_by_count: 20
     },
+];
+
+const mockResolvedTop5Papers = [
+    {
+        id: "386866",
+        openalex_id: "W2741809807",
+        title: "The state of OA: a large-scale analysis of the prevalence and impact of Open Access articles",
+        display_name: "The state of OA: a large-scale analysis of the prevalence and impact of Open Access articles",
+        abstract: "Despite growing interest in Open Access (OA) to scholarly literature, ...",
+        publication_year: 2018,
+        cited_by_count: 1193,
+        fwci: 490.1635,
+        primary_source_display_name: "PeerJ",
+        primary_topic_display_name: "scientometrics and bibliometrics research",
+        is_open_access: true,
+        open_access_status: "gold",
+        author_count: 9,
+        authors_preview: [
+            { id: "A5048491430", name: "Heather Piwowar" },
+            { id: "A5023888391", name: "Jason Priem" }
+        ]
+    },
+    {
+        id: "891407",
+        openalex_id: "W2046766973",
+        title: "Sharing Detailed Research Data Is Associated with Increased Citation Rate",
+        display_name: "Sharing Detailed Research Data Is Associated with Increased Citation Rate",
+        abstract: "BACKGROUND: Sharing research data provides benefit to the general scientific community, ...",
+        publication_year: 2007,
+        cited_by_count: 904,
+        fwci: 48.0677,
+        primary_source_display_name: "PLoS ONE",
+        primary_topic_display_name: "Research Data Management Practices",
+        is_open_access: true,
+        open_access_status: "gold",
+        author_count: 3,
+        authors_preview: [
+            { id: "A5048491430", name: "Heather Piwowar" },
+            { id: "A5109883106", name: "Roger Day" }
+        ]
+    },
+    {
+        id: "386900",
+        openalex_id: "W2045657963",
+        title: "Data reuse and the open data citation advantage",
+        display_name: "Data reuse and the open data citation advantage",
+        abstract: "Background. Attribution to the original contributor upon reuse of published data is important both as a reward for data creators and to document the provenance of research findings. Previous studies have found that ...",
+        publication_year: 2013,
+        cited_by_count: 614,
+        fwci: 92.7518,
+        primary_source_display_name: "PeerJ",
+        primary_topic_display_name: "Research Data Management Practices",
+        is_open_access: true,
+        open_access_status: "gold",
+        author_count: 2,
+        authors_preview: [
+            { id: "A5048491430", name: "Heather Piwowar" },
+            { id: "A5013911206", name: "Todd Vision" }
+        ]
+    },
+    {
+        id: "386943",
+        openalex_id: "W1572136682",
+        title: "Value all research products",
+        display_name: "Value all research products",
+        abstract: null,
+        publication_year: 2013,
+        cited_by_count: 410,
+        fwci: 125.3335,
+        primary_source_display_name: "Nature",
+        primaryT_topic_display_name: "Research Data Management Practices",
+        is_open_access: true,
+        open_access_status: "bronze",
+        author_count: 1,
+        authors_preview: [
+            { id: "A5048491430", name: "Heather Piwowar" }
+        ]
+    },
+    {
+        id: "217473",
+        openalex_id: "W1553564559",
+        title: "Altmetrics in the wild: Using social media to explore scholarly impact",
+        display_name: "Altmetrics in the wild: Using social media to explore scholarly impact",
+        abstract: "In growing numbers, scholars are integrating social media tools like blogs, Twitter, and Mendeley into their professional communications. The online, public nature of these tools exposes and reifies scholarly processes once hidden and ephemeral. Metrics based on this activities could inform broader, faster measures of impact, complementing traditional citation metrics. This study explores the properties of these social media-based metrics or \"altmetrics\", sampling 24,331 articles published by the Public Library of Science. We find that that different indicators vary greatly in activity. Around 5% of sampled articles are cited in Wikipedia, while close to 80% have been included in at least one Mendeley library. There is, however, an encouraging diversity; a quarter of articles have nonzero data from five or more different sources. Correlation and factor analysis suggest citation and altmetrics indicators track related but distinct impacts, with neither able to describe the complete picture of scholarly use alone. There are moderate correlations between Mendeley and Web of Science citation, but many altmetric indicators seem to measure impact mostly orthogonal to citation. Articles cluster in ways that suggest five different impact \"flavors\", capturing impacts of different types on different audiences; for instance, some articles may be heavily read and saved by scholars but seldom cited. Together, these findings encourage more research into altmetrics as complements to traditional citation measures.",
+        publication_year: 2012,
+        cited_by_count: 360,
+        fwci: 0,
+        primary_source_display_name: "arXiv (Cornell University)",
+        primary_topic_display_name: "scientometrics and bibliometrics research",
+        is_open_access: true,
+        open_access_status: "green",
+        author_count: 3,
+        authors_preview: [
+            { id: "A5023888391", name: "Jason Priem" },
+            { id: "A5048491430", name: "Heather Piwowar" }
+        ]
+    }
 ];
 
 
@@ -510,6 +608,71 @@ describe("fetchAuthorCountsByYear", () => {
         const [query, params] = pool.query.mock.calls[0];
 
         expectFetchAuthorCountsByYearQuery(query);
+        expect(params).toEqual(["50703"]);
+    });
+});
+
+
+function expectFetchAuthorTop5PapersQuery(query) {
+    expect(query).toContain("SELECT");
+    expect(query).toContain("FROM papers p");
+    expect(query).toContain("JOIN paper_authors pa_target");
+    expect(query).toContain("ON pa_target.paper_id = p.id");
+    expect(query).toContain("LEFT JOIN paper_authors pa_all");
+    expect(query).toContain("ON pa_all.paper_id = p.id");
+    expect(query).toContain("WHERE pa_target.author_id = $1");
+    expect(query).toContain("GROUP BY p.id");
+    expect(query).toContain("ORDER BY p.cited_by_count DESC NULLS LAST");
+    expect(query).toContain("LIMIT 5;");
+}
+
+describe("fetchAuthorTop5Papers", () => {
+    beforeEach(() => {
+        vi.resetAllMocks();
+    });
+
+    it("Fetches the top 5 top papers associated with a particular author", async () => {
+        pool.query.mockResolvedValue({
+            rows: mockResolvedTop5Papers
+        });
+
+        const results = await fetchAuthorTop5Papers("50703");
+
+        const [query, params] = pool.query.mock.calls[0];
+
+        expectFetchAuthorTop5PapersQuery(query);
+        expect(params).toEqual(["50703"]);
+
+        expect(results).toEqual(mockResolvedTop5Papers);
+    });
+
+    it("Fetches an empty array for an invalid internal author id", async () => {
+        pool.query.mockResolvedValue({
+            rows: []
+        });
+
+        const results = await fetchAuthorTop5Papers("50703");
+
+        const [query, params] = pool.query.mock.calls[0];
+
+        expectFetchAuthorTop5PapersQuery(query);
+        expect(params).toEqual(["50703"]);
+
+        expect(results).toEqual([]);
+    });
+
+    // ---------- DB ERROR ----------
+    
+    it("An unexpected database error occurs", async () => {
+        pool.query.mockRejectedValue(new Error("Unexpected DB error"));
+
+        await expect(fetchAuthorTop5Papers("50703"))
+        .rejects
+        .toThrow("Unexpected DB error");
+
+        const [query, params] = pool.query.mock.calls[0];
+
+        expectFetchAuthorTop5PapersQuery(query);
         expect(params).toEqual(["50703"]);
     });
 });
--- server/tests/unit/authorService.test.js
+++ server/tests/unit/authorService.test.js
@@ -7,7 +7,8 @@ vi.mock("./../../src/repositories/authorRepository.js", () => ({
     fetchAuthorLastKnownInstitutionsById: vi.fn(),
     fetchAuthorTopicsById: vi.fn(),
     fetchAuthorTopicSharesById: vi.fn(),
-    fetchAuthorCountsByYearById: vi.fn()
+    fetchAuthorCountsByYearById: vi.fn(),
+    fetchAuthorTop5Papers: vi.fn()
 }));
 
 // Import after to replace the real function with the mock function
@@ -17,7 +18,8 @@ import {
     fetchAuthorLastKnownInstitutionsById,
     fetchAuthorTopicsById,
     fetchAuthorTopicSharesById,
-    fetchAuthorCountsByYearById } from "../../src/repositories/authorRepository.js";
+    fetchAuthorCountsByYearById, 
+    fetchAuthorTop5Papers } from "../../src/repositories/authorRepository.js";
 import { getAuthorById } from "../../src/services/authorService.js";
 
 
@@ -132,6 +134,103 @@ const mockResolvedAuthorCountsByYear = [
     },
 ];
 
+const mockResolvedTop5Papers = [
+    {
+        id: "386866",
+        openalex_id: "W2741809807",
+        title: "The state of OA: a large-scale analysis of the prevalence and impact of Open Access articles",
+        display_name: "The state of OA: a large-scale analysis of the prevalence and impact of Open Access articles",
+        abstract: "Despite growing interest in Open Access (OA) to scholarly literature, ...",
+        publication_year: 2018,
+        cited_by_count: 1193,
+        fwci: 490.1635,
+        primary_source_display_name: "PeerJ",
+        primary_topic_display_name: "scientometrics and bibliometrics research",
+        is_open_access: true,
+        open_access_status: "gold",
+        author_count: 9,
+        authors_preview: [
+            { id: "A5048491430", name: "Heather Piwowar" },
+            { id: "A5023888391", name: "Jason Priem" }
+        ]
+    },
+    {
+        id: "891407",
+        openalex_id: "W2046766973",
+        title: "Sharing Detailed Research Data Is Associated with Increased Citation Rate",
+        display_name: "Sharing Detailed Research Data Is Associated with Increased Citation Rate",
+        abstract: "BACKGROUND: Sharing research data provides benefit to the general scientific community, ...",
+        publication_year: 2007,
+        cited_by_count: 904,
+        fwci: 48.0677,
+        primary_source_display_name: "PLoS ONE",
+        primary_topic_display_name: "Research Data Management Practices",
+        is_open_access: true,
+        open_access_status: "gold",
+        author_count: 3,
+        authors_preview: [
+            { id: "A5048491430", name: "Heather Piwowar" },
+            { id: "A5109883106", name: "Roger Day" }
+        ]
+    },
+    {
+        id: "386900",
+        openalex_id: "W2045657963",
+        title: "Data reuse and the open data citation advantage",
+        display_name: "Data reuse and the open data citation advantage",
+        abstract: "Background. Attribution to the original contributor upon reuse of published data is important both as a reward for data creators and to document the provenance of research findings. Previous studies have found that ...",
+        publication_year: 2013,
+        cited_by_count: 614,
+        fwci: 92.7518,
+        primary_source_display_name: "PeerJ",
+        primary_topic_display_name: "Research Data Management Practices",
+        is_open_access: true,
+        open_access_status: "gold",
+        author_count: 2,
+        authors_preview: [
+            { id: "A5048491430", name: "Heather Piwowar" },
+            { id: "A5013911206", name: "Todd Vision" }
+        ]
+    },
+    {
+        id: "386943",
+        openalex_id: "W1572136682",
+        title: "Value all research products",
+        display_name: "Value all research products",
+        abstract: null,
+        publication_year: 2013,
+        cited_by_count: 410,
+        fwci: 125.3335,
+        primary_source_display_name: "Nature",
+        primaryT_topic_display_name: "Research Data Management Practices",
+        is_open_access: true,
+        open_access_status: "bronze",
+        author_count: 1,
+        authors_preview: [
+            { id: "A5048491430", name: "Heather Piwowar" }
+        ]
+    },
+    {
+        id: "217473",
+        openalex_id: "W1553564559",
+        title: "Altmetrics in the wild: Using social media to explore scholarly impact",
+        display_name: "Altmetrics in the wild: Using social media to explore scholarly impact",
+        abstract: "In growing numbers, scholars are integrating social media tools like blogs, Twitter, and Mendeley into their professional communications. The online, public nature of these tools exposes and reifies scholarly processes once hidden and ephemeral. Metrics based on this activities could inform broader, faster measures of impact, complementing traditional citation metrics. This study explores the properties of these social media-based metrics or \"altmetrics\", sampling 24,331 articles published by the Public Library of Science. We find that that different indicators vary greatly in activity. Around 5% of sampled articles are cited in Wikipedia, while close to 80% have been included in at least one Mendeley library. There is, however, an encouraging diversity; a quarter of articles have nonzero data from five or more different sources. Correlation and factor analysis suggest citation and altmetrics indicators track related but distinct impacts, with neither able to describe the complete picture of scholarly use alone. There are moderate correlations between Mendeley and Web of Science citation, but many altmetric indicators seem to measure impact mostly orthogonal to citation. Articles cluster in ways that suggest five different impact \"flavors\", capturing impacts of different types on different audiences; for instance, some articles may be heavily read and saved by scholars but seldom cited. Together, these findings encourage more research into altmetrics as complements to traditional citation measures.",
+        publication_year: 2012,
+        cited_by_count: 360,
+        fwci: 0,
+        primary_source_display_name: "arXiv (Cornell University)",
+        primary_topic_display_name: "scientometrics and bibliometrics research",
+        is_open_access: true,
+        open_access_status: "green",
+        author_count: 3,
+        authors_preview: [
+            { id: "A5023888391", name: "Jason Priem" },
+            { id: "A5048491430", name: "Heather Piwowar" }
+        ]
+    }
+];
+
 describe("getAuthorById", () => {
     // Reseting the mock's call history before every test
     beforeEach(() => {
@@ -145,6 +244,7 @@ describe("getAuthorById", () => {
         fetchAuthorTopicsById.mockResolvedValue(mockResolvedAuthorTopics);
         fetchAuthorTopicSharesById.mockResolvedValue(mockResolvedAuthorTopicShares);
         fetchAuthorCountsByYearById.mockResolvedValue(mockResolvedAuthorCountsByYear);
+        fetchAuthorTop5Papers.mockResolvedValue(mockResolvedTop5Papers);
         
         const expectedOutput = {
             id: mockResolvedAuthor.openalex_id,
@@ -215,6 +315,23 @@ describe("getAuthorById", () => {
                 worksCount: count.works_count,
                 oaWorksCount: count.oa_works_count,
                 citedByCount: count.cited_by_count
+            })),
+
+            topPapers: mockResolvedTop5Papers.map(paper => ({
+                id: paper.openalex_id,
+                internalId: paper.id,
+                title: paper.title,
+                displayName: paper.display_name,
+                abstract: paper.abstract,
+                publicationYear: paper.publication_year,
+                citedByCount: paper.cited_by_count,
+                fwci: Number(paper.fwci),
+                primarySource: paper.primary_source_display_name,
+                primaryTopic: paper.primary_topic_display_name,
+                isOpenAccess: paper.is_open_access,
+                openAccessStatus: paper.open_access_status,
+                authorCount: Number(paper.author_count),
+                authorsPreview: paper.authors_preview,
             }))
         };
 
@@ -238,6 +355,9 @@ describe("getAuthorById", () => {
 
         expect(fetchAuthorCountsByYearById).toHaveBeenCalledWith("50703");
         expect(fetchAuthorCountsByYearById).toHaveBeenCalledTimes(1);
+
+        expect(fetchAuthorTop5Papers).toHaveBeenCalledWith("50703");
+        expect(fetchAuthorTop5Papers).toHaveBeenCalledTimes(1);
 
         expect(result).toEqual(expectedOutput);
     });
@@ -266,6 +386,7 @@ describe("getAuthorById", () => {
         expect(fetchAuthorTopicsById).not.toHaveBeenCalled();
         expect(fetchAuthorTopicSharesById).not.toHaveBeenCalled();
         expect(fetchAuthorCountsByYearById).not.toHaveBeenCalled();  
+        expect(fetchAuthorTop5Papers).not.toHaveBeenCalled();
     });
 
 
@@ -281,6 +402,7 @@ describe("getAuthorById", () => {
         expect(fetchAuthorTopicsById).not.toHaveBeenCalled();
         expect(fetchAuthorTopicSharesById).not.toHaveBeenCalled();
         expect(fetchAuthorCountsByYearById).not.toHaveBeenCalled();  
+        expect(fetchAuthorTop5Papers).not.toHaveBeenCalled();
     });
 
     // ------------ DATABASE ERRORS --------------
@@ -297,6 +419,7 @@ describe("getAuthorById", () => {
         expect(fetchAuthorLastKnownInstitutionsById).not.toHaveBeenCalled();
         expect(fetchAuthorTopicsById).not.toHaveBeenCalled();
         expect(fetchAuthorTopicSharesById).not.toHaveBeenCalled();
-        expect(fetchAuthorCountsByYearById).not.toHaveBeenCalled(); 
+        expect(fetchAuthorCountsByYearById).not.toHaveBeenCalled();
+        expect(fetchAuthorTop5Papers).not.toHaveBeenCalled(); 
     });
 });
