33231300d232d9173a2cff2f4cfc22dac66541d0
Apostolis Falaras <apostolisfalaras@gmail.com>
2026-06-12 12:13:18 +0300

    Update fetchAuthorPapers repository tests with pagination-based tests

--- server/tests/unit/authorRepository.test.js
+++ server/tests/unit/authorRepository.test.js
@@ -16,7 +16,7 @@ import {
     fetchAuthorTopicsById,
     fetchAuthorTopicSharesById,
     fetchAuthorCountsByYearById,
-    fetchAuthorTop5Papers } from "../../src/repositories/authorRepository.js";
+    fetchAuthorPapers } from "../../src/repositories/authorRepository.js";
 
 
 const mockResolvedAuthor = {
@@ -149,7 +149,7 @@ const mockResolvedAuthorCountsByYear = [
     },
 ];
 
-const mockResolvedTop5Papers = [
+const mockResolvedPapers1 = [
     {
         id: "386866",
         openalex_id: "W2741809807",
@@ -246,6 +246,103 @@ const mockResolvedTop5Papers = [
     }
 ];
 
+const mockResolvedPapers2 = [
+    
+    {
+        id: "387108",
+        openalex_id: "W2066415719",
+        title: "Who Shares? Who Doesn't? Factors Associated with Openly Archiving Raw Research Data",
+        display_name: "Who Shares? Who Doesn't? Factors Associated with Openly Archiving Raw Research Data",
+        abstract: "Many initiatives encourage investigators to share their raw datasets in hopes of increasing research efficiency and quality. Despite these investments of time and money, we do not have a firm grasp of who openly shares raw research data, who doesn't, and which initiatives are correlated with high rates of data sharing. In this analysis I use bibliometric methods to identify patterns in the frequency with which investigators openly archive their raw gene expression microarray datasets after study publication. Automated methods identified 11,603 articles published between 2000 and 2009 that describe the creation of gene expression microarray data. Associated datasets in best-practice repositories were found for 25% of these articles, increasing from less than 5% in 2001 to 30%-35% in 2007-2009. Accounting for sensitivity of the automated methods, approximately 45% of recent gene expression studies made their data publicly available. First-order factor analysis on 124 diverse bibliometric attributes of the data creation articles revealed 15 factors describing authorship, funding, institution, publication, and domain environments. In multivariate regression, authors were most likely to share data if they had prior experience sharing or reusing data, if their study was published in an open access journal or a journal with a relatively strong data sharing policy, or if the study was funded by a large number of NIH grants. Authors of studies on cancer and human subjects were least likely to make their datasets available. These results suggest research data sharing levels are still low and increasing only slowly, and data is least available in areas where it could make the biggest impact. Let's learn from those with high rates of sharing to embrace the full potential of our research output.",
+        publication_year: 2011,
+        cited_by_count: 216,
+        fwci: 45.1038,
+        primary_source_display_name: "PLoS ONE",
+        primary_topic_display_name: "Research Data Management Practices",
+        is_open_access: true,
+        open_access_status: "gold",
+        author_count: 1,
+        authors_preview: [
+            { id: "A5048491430", name: "Heather Piwowar" }
+        ]
+    },
+    {
+        id: "387266",
+        opeanlex_id: "W2170531319",
+        title: "Towards a Data Sharing Culture: Recommendations for Leadership from Academic Health Centers",
+        display_name: "Towards a Data Sharing Culture: Recommendations for Leadership from Academic Health Centers",
+        abstract: "haring biomedical research and health care data is important but difficult. Recognizing this, many initiatives facilitate, fund, request, or require researchers to share their data [1] These initiatives address the technical aspects of data sharing, but rarely focus on incentives for key stakeholders Academic health centers (AHCs) have a critical role in enabling, encouraging, and rewarding data sharing. The leaders of medical schools and academic-affiliated hospitals can play a unique role in supporting this transformation of the research enterprise. We propose that AHCs can and should lead the transition towards a culture of biomedical data sharing.",
+        publication_year: 2008,
+        cited_by_count: 153,
+        fwci: 26.2434,
+        primary_source_display_name: "PLoS Medicine",
+        primary_topic_display_name: "Research Data Management Practices",
+        is_open_access: true,
+        open_access_status: "gold",
+        author_count: 4,
+        authors_preview: [
+            { id: "A5048491430", name: "Heather Piwowar" },
+            { id: "A5085379143", name: "Michael J. Becich" }
+        ]
+    },
+    {
+        id: "387321",
+        openalex_id: "W1963524534",
+        title: "Public sharing of research datasets: A pilot study of associations",
+        display_name: "Public sharing of research datasets: A pilot study of associations",
+        abstract: null,
+        publication_year: 2009,
+        cited_by_count: 142,
+        fwci: 10.0924,
+        primary_source_display_name: "Journal of Informetrics",
+        primary_topic_display_name: "Research Data Management Practices",
+        is_open_access: true,
+        open_access_status: "green",
+        author_count: 2,
+        authors_preview: [
+            { id: "A5048491430", name: "Heather Piwowar" },
+            { id: "A5065170642", name: "Wendy W. Chapman" }
+        ]
+    },
+    {
+        id: "387562",
+        openalex_id: "W2003014790",
+        title: "Data archiving is a good investment",
+        display_name: "Data archiving is a good investment",
+        abstract: null,
+        publication_year: 2011,
+        cited_by_count: 108,
+        fwci: 27.5204,
+        primary_source_display_name: "Nature",
+        primary_topic_display_name: "Research Data Management Practices",
+        is_open_access: true,
+        open_accessS_status: "bronze",
+        author_count: 3,
+        authors_preview: [
+            { id: "A5048491430", name: "Heather Piwowar" },
+            { id: "A5013911206", name: "Todd Vision" }
+        ]
+    },
+    {
+        id: "387648",
+        openalex_id: "W2980172586",
+        title: "The Future of OA: A large-scale analysis projecting Open Access publication and readership",
+        display_name: "The Future of OA: A large-scale analysis projecting Open Access publication and readership",
+        abstract: "Summary Understanding the growth of open access (OA) is important for deciding funder policy, subscription allocation, and infrastructure planning. This study analyses the number of papers available as OA over time. The models includes both OA embargo data and the relative growth rates of different OA types over time, based on the OA status of 70 million journal articles published between 1950 and 2019. The study also looks at article usage data, analyzing the proportion of views to OA articles vs views to articles which are closed access. Signal processing techniques are used to model how these viewership patterns change over time. Viewership data is based on 2.8 million uses of the Unpaywall browser extension in July 2019. We found that Green, Gold, and Hybrid papers receive more views than their Closed or Bronze counterparts, particularly Green papers made available within a year of publication. We also found that the proportion of Green, Gold, and Hybrid articles is growing most quickly. In 2019: 31% of all journal articles are available as OA 52% of article views are to OA articles Given existing trends, we estimate that by 2025: 44% of all journal articles will be available as OA 70% of article views will be to OA articles The declining relevance of closed access articles is likely to change the landscape of scholarly communication in the years to come. Percent of views, by OA type: Percent of papers, by OA type:",
+        publication_year: 2019,
+        cited_by_count: 100,
+        fwci: 0,
+        primary_source_display_name: "bioRxiv (Cold Spring Harbor Laboratory)",
+        primary_topic_display_name: "scientometrics and bibliometrics research",
+        is_open_access: true,
+        open_access_status: "green",
+        author_count: 3,
+        authors_preview: [
+            { id: "A5048491430", name: "Heather Piwowar" },
+            { id: "A5023888391", name: "Jason Priem" }
+        ]
+    }
+];
 
 function expectFetchAuthorQuery(query) {
     expect(query).toContain("SELECT *");
@@ -613,7 +710,7 @@ describe("fetchAuthorCountsByYear", () => {
 });
 
 
-function expectFetchAuthorTop5PapersQuery(query) {
+function expectFetchAuthorPapersQuery(query) {
     expect(query).toContain("SELECT");
     expect(query).toContain("FROM papers p");
     expect(query).toContain("JOIN paper_authors pa_target");
@@ -623,40 +720,81 @@ function expectFetchAuthorTop5PapersQuery(query) {
     expect(query).toContain("WHERE pa_target.author_id = $1");
     expect(query).toContain("GROUP BY p.id");
     expect(query).toContain("ORDER BY p.cited_by_count DESC NULLS LAST");
-    expect(query).toContain("LIMIT 5;");
+    expect(query).toContain("LIMIT $2");
+    expect(query).toContain("OFFSET $3;");
 }
 
-describe("fetchAuthorTop5Papers", () => {
+describe("fetchAuthorPapers", () => {
     beforeEach(() => {
         vi.resetAllMocks();
     });
 
     it("Fetches the top 5 top papers associated with a particular author", async () => {
         pool.query.mockResolvedValue({
-            rows: mockResolvedTop5Papers
+            rows: mockResolvedPapers1
         });
 
-        const results = await fetchAuthorTop5Papers("50703");
+        const results = await fetchAuthorPapers("50703", 5, 0);
 
         const [query, params] = pool.query.mock.calls[0];
 
-        expectFetchAuthorTop5PapersQuery(query);
-        expect(params).toEqual(["50703"]);
+        expectFetchAuthorPapersQuery(query);
+        expect(params).toEqual(["50703", 5, 0]);
 
-        expect(results).toEqual(mockResolvedTop5Papers);
+        expect(results).toEqual(mockResolvedPapers1);
     });
+
+    it("Fetches an authors associated papers using default pagination", async () => {
+        pool.query.mockResolvedValue({
+            rows: [
+                ...mockResolvedPapers1,
+                ...mockResolvedPapers2
+            ]
+        });
+
+        // Default pagination: 10 papers from 1st page of papers
+        // LIMIT -> 10, OFFSET -> 0
+        const results = await fetchAuthorPapers("50703", 10, 0);
+
+        const [query, params] = pool.query.mock.calls[0];
+
+        expectFetchAuthorPapersQuery(query);
+        expect(params).toEqual(["50703", 10, 0]);
+
+        expect(results).toEqual([...mockResolvedPapers1, ...mockResolvedPapers2]);
+    });
+
+    it("Fetches an authors associated papers using custom pagination", async () => {
+        pool.query.mockResolvedValue({
+            rows: [
+                ...mockResolvedPapers2
+            ]
+        });
+
+        // Custom pagination: 5 paper from 2nd page of papers, 
+        // LIMIT -> 5, OFFSET -> 5
+        const results = await fetchAuthorPapers("50703", 5, 5);
+
+        const [query, params] = pool.query.mock.calls[0];
+
+        expectFetchAuthorPapersQuery(query);
+        expect(params).toEqual(["50703", 5, 5]);
+
+        expect(results).toEqual(mockResolvedPapers2);
+    });
+
 
     it("Fetches an empty array for an invalid internal author id", async () => {
         pool.query.mockResolvedValue({
             rows: []
         });
 
-        const results = await fetchAuthorTop5Papers("50703");
+        const results = await fetchAuthorPapers("50703", 5, 0);
 
         const [query, params] = pool.query.mock.calls[0];
 
-        expectFetchAuthorTop5PapersQuery(query);
-        expect(params).toEqual(["50703"]);
+        expectFetchAuthorPapersQuery(query);
+        expect(params).toEqual(["50703", 5, 0]);
 
         expect(results).toEqual([]);
     });
@@ -666,13 +804,13 @@ describe("fetchAuthorTop5Papers", () => {
     it("An unexpected database error occurs", async () => {
         pool.query.mockRejectedValue(new Error("Unexpected DB error"));
 
-        await expect(fetchAuthorTop5Papers("50703"))
+        await expect(fetchAuthorPapers("50703", 5, 0))
         .rejects
         .toThrow("Unexpected DB error");
 
         const [query, params] = pool.query.mock.calls[0];
 
-        expectFetchAuthorTop5PapersQuery(query);
-        expect(params).toEqual(["50703"]);
+        expectFetchAuthorPapersQuery(query);
+        expect(params).toEqual(["50703", 5, 0]);
     });
 });
