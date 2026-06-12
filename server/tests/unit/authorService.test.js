ce976ae642ae8286cc1fb191a11067c8be28e7a9
Apostolis Falaras <apostolisfalaras@gmail.com>
2026-06-12 12:04:54 +0300

    Update author service unit tests with the getAuthorPaper tests

--- server/tests/unit/authorService.test.js
+++ server/tests/unit/authorService.test.js
@@ -8,7 +8,7 @@ vi.mock("./../../src/repositories/authorRepository.js", () => ({
     fetchAuthorTopicsById: vi.fn(),
     fetchAuthorTopicSharesById: vi.fn(),
     fetchAuthorCountsByYearById: vi.fn(),
-    fetchAuthorTop5Papers: vi.fn()
+    fetchAuthorPapers: vi.fn()
 }));
 
 // Import after to replace the real function with the mock function
@@ -19,8 +19,8 @@ import {
     fetchAuthorTopicsById,
     fetchAuthorTopicSharesById,
     fetchAuthorCountsByYearById, 
-    fetchAuthorTop5Papers } from "../../src/repositories/authorRepository.js";
-import { getAuthorById } from "../../src/services/authorService.js";
+    fetchAuthorPapers } from "../../src/repositories/authorRepository.js";
+import { getAuthorById, getAuthorPapers } from "../../src/services/authorService.js";
 
 
 const mockResolvedAuthor = {
@@ -134,7 +134,7 @@ const mockResolvedAuthorCountsByYear = [
     },
 ];
 
-const mockResolvedTop5Papers = [
+const mockResolvedPapers1 = [
     {
         id: "386866",
         openalex_id: "W2741809807",
@@ -231,6 +231,104 @@ const mockResolvedTop5Papers = [
     }
 ];
 
+const mockResolvedPapers2 = [
+    
+    {
+        id: "387108",
+        openalex_id: "W2066415719",
+        title: "Who Shares? Who Doesn't? Factors Associated with Openly Archiving Raw Research Data",
+        display_name: "Who Shares? Who Doesn't? Factors Associated with Openly Archiving Raw Research Data",
+        abstract: "Many initiatives encourage investigators to share their raw datasets in hopes of increasing research efficiency and quality. Despite these investments of time and money, we do not have a firm grasp of who openly shares raw research data, who doesn't, and which initiatives are correlated with high rates of data sharing. In this analysis I use bibliometric methods to identify patterns in the frequency with which investigators openly archive their raw gene expression microarray datasets after study publication. Automated methods identified 11,603 articles published between 2000 and 2009 that describe the creation of gene expression microarray data. Associated datasets in best-practice repositories were found for 25% of these articles, increasing from less than 5% in 2001 to 30%-35% in 2007-2009. Accounting for sensitivity of the automated methods, approximately 45% of recent gene expression studies made their data publicly available. First-order factor analysis on 124 diverse bibliometric attributes of the data creation articles revealed 15 factors describing authorship, funding, institution, publication, and domain environments. In multivariate regression, authors were most likely to share data if they had prior experience sharing or reusing data, if their study was published in an open access journal or a journal with a relatively strong data sharing policy, or if the study was funded by a large number of NIH grants. Authors of studies on cancer and human subjects were least likely to make their datasets available. These results suggest research data sharing levels are still low and increasing only slowly, and data is least available in areas where it could make the biggest impact. Let's learn from those with high rates of sharing to embrace the full potential of our research output.",
+        publication_year: 2011,
+        cited_by_count: 216,
+        fwci: 45.1038,
+        primary_source_display_name: "PLoS ONE",
+        primary_topic_display_name: "Research Data Management Practices",
+        is_open_access: true,
+        open_access_status: "gold",
+        author_count: 1,
+        authors_preview: [
+            { id: "A5048491430", name: "Heather Piwowar" }
+        ]
+    },
+    {
+        id: "387266",
+        opeanlex_id: "W2170531319",
+        title: "Towards a Data Sharing Culture: Recommendations for Leadership from Academic Health Centers",
+        display_name: "Towards a Data Sharing Culture: Recommendations for Leadership from Academic Health Centers",
+        abstract: "haring biomedical research and health care data is important but difficult. Recognizing this, many initiatives facilitate, fund, request, or require researchers to share their data [1] These initiatives address the technical aspects of data sharing, but rarely focus on incentives for key stakeholders Academic health centers (AHCs) have a critical role in enabling, encouraging, and rewarding data sharing. The leaders of medical schools and academic-affiliated hospitals can play a unique role in supporting this transformation of the research enterprise. We propose that AHCs can and should lead the transition towards a culture of biomedical data sharing.",
+        publication_year: 2008,
+        cited_by_count: 153,
+        fwci: 26.2434,
+        primary_source_display_name: "PLoS Medicine",
+        primary_topic_display_name: "Research Data Management Practices",
+        is_open_access: true,
+        open_access_status: "gold",
+        author_count: 4,
+        authors_preview: [
+            { id: "A5048491430", name: "Heather Piwowar" },
+            { id: "A5085379143", name: "Michael J. Becich" }
+        ]
+    },
+    {
+        id: "387321",
+        openalex_id: "W1963524534",
+        title: "Public sharing of research datasets: A pilot study of associations",
+        display_name: "Public sharing of research datasets: A pilot study of associations",
+        abstract: null,
+        publication_year: 2009,
+        cited_by_count: 142,
+        fwci: 10.0924,
+        primary_source_display_name: "Journal of Informetrics",
+        primary_topic_display_name: "Research Data Management Practices",
+        is_open_access: true,
+        open_access_status: "green",
+        author_count: 2,
+        authors_preview: [
+            { id: "A5048491430", name: "Heather Piwowar" },
+            { id: "A5065170642", name: "Wendy W. Chapman" }
+        ]
+    },
+    {
+        id: "387562",
+        openalex_id: "W2003014790",
+        title: "Data archiving is a good investment",
+        display_name: "Data archiving is a good investment",
+        abstract: null,
+        publication_year: 2011,
+        cited_by_count: 108,
+        fwci: 27.5204,
+        primary_source_display_name: "Nature",
+        primary_topic_display_name: "Research Data Management Practices",
+        is_open_access: true,
+        open_accessS_status: "bronze",
+        author_count: 3,
+        authors_preview: [
+            { id: "A5048491430", name: "Heather Piwowar" },
+            { id: "A5013911206", name: "Todd Vision" }
+        ]
+    },
+    {
+        id: "387648",
+        openalex_id: "W2980172586",
+        title: "The Future of OA: A large-scale analysis projecting Open Access publication and readership",
+        display_name: "The Future of OA: A large-scale analysis projecting Open Access publication and readership",
+        abstract: "Summary Understanding the growth of open access (OA) is important for deciding funder policy, subscription allocation, and infrastructure planning. This study analyses the number of papers available as OA over time. The models includes both OA embargo data and the relative growth rates of different OA types over time, based on the OA status of 70 million journal articles published between 1950 and 2019. The study also looks at article usage data, analyzing the proportion of views to OA articles vs views to articles which are closed access. Signal processing techniques are used to model how these viewership patterns change over time. Viewership data is based on 2.8 million uses of the Unpaywall browser extension in July 2019. We found that Green, Gold, and Hybrid papers receive more views than their Closed or Bronze counterparts, particularly Green papers made available within a year of publication. We also found that the proportion of Green, Gold, and Hybrid articles is growing most quickly. In 2019: 31% of all journal articles are available as OA 52% of article views are to OA articles Given existing trends, we estimate that by 2025: 44% of all journal articles will be available as OA 70% of article views will be to OA articles The declining relevance of closed access articles is likely to change the landscape of scholarly communication in the years to come. Percent of views, by OA type: Percent of papers, by OA type:",
+        publication_year: 2019,
+        cited_by_count: 100,
+        fwci: 0,
+        primary_source_display_name: "bioRxiv (Cold Spring Harbor Laboratory)",
+        primary_topic_display_name: "scientometrics and bibliometrics research",
+        is_open_access: true,
+        open_access_status: "green",
+        author_count: 3,
+        authors_preview: [
+            { id: "A5048491430", name: "Heather Piwowar" },
+            { id: "A5023888391", name: "Jason Priem" }
+        ]
+    }
+];
+
 describe("getAuthorById", () => {
     // Reseting the mock's call history before every test
     beforeEach(() => {
@@ -244,7 +342,7 @@ describe("getAuthorById", () => {
         fetchAuthorTopicsById.mockResolvedValue(mockResolvedAuthorTopics);
         fetchAuthorTopicSharesById.mockResolvedValue(mockResolvedAuthorTopicShares);
         fetchAuthorCountsByYearById.mockResolvedValue(mockResolvedAuthorCountsByYear);
-        fetchAuthorTop5Papers.mockResolvedValue(mockResolvedTop5Papers);
+        fetchAuthorPapers.mockResolvedValue(mockResolvedPapers1);
         
         const expectedOutput = {
             id: mockResolvedAuthor.openalex_id,
@@ -317,7 +415,7 @@ describe("getAuthorById", () => {
                 citedByCount: count.cited_by_count
             })),
 
-            topPapers: mockResolvedTop5Papers.map(paper => ({
+            topPapers: mockResolvedPapers1.map(paper => ({
                 id: paper.openalex_id,
                 internalId: paper.id,
                 title: paper.title,
@@ -356,8 +454,8 @@ describe("getAuthorById", () => {
         expect(fetchAuthorCountsByYearById).toHaveBeenCalledWith("50703");
         expect(fetchAuthorCountsByYearById).toHaveBeenCalledTimes(1);
 
-        expect(fetchAuthorTop5Papers).toHaveBeenCalledWith("50703");
-        expect(fetchAuthorTop5Papers).toHaveBeenCalledTimes(1);
+        expect(fetchAuthorPapers).toHaveBeenCalledWith("50703", 5, 0);
+        expect(fetchAuthorPapers).toHaveBeenCalledTimes(1);
 
         expect(result).toEqual(expectedOutput);
     });
@@ -373,7 +471,8 @@ describe("getAuthorById", () => {
         expect(fetchAuthorLastKnownInstitutionsById).not.toHaveBeenCalled();
         expect(fetchAuthorTopicsById).not.toHaveBeenCalled();
         expect(fetchAuthorTopicSharesById).not.toHaveBeenCalled();
-        expect(fetchAuthorCountsByYearById).not.toHaveBeenCalled(); 
+        expect(fetchAuthorCountsByYearById).not.toHaveBeenCalled();
+        expect(fetchAuthorPapers).not.toHaveBeenCalled(); 
     });
 
     it("Throws a 400 AppError when input id has invalid format", async () => {
@@ -386,12 +485,14 @@ describe("getAuthorById", () => {
         expect(fetchAuthorTopicsById).not.toHaveBeenCalled();
         expect(fetchAuthorTopicSharesById).not.toHaveBeenCalled();
         expect(fetchAuthorCountsByYearById).not.toHaveBeenCalled();  
-        expect(fetchAuthorTop5Papers).not.toHaveBeenCalled();
+        expect(fetchAuthorPapers).not.toHaveBeenCalled();
     });
 
 
     it("Throws a 404 AppError when author doesn't exist", async () => {
-        // Testing invalid paper ID input
+        fetchAuthorById.mockResolvedValue(null);
+
+        // Testing invalid author ID input
         await expect(getAuthorById("A5107860")).rejects.toThrow("Author not found");
         
         expect(fetchAuthorById).toHaveBeenCalledWith("A5107860");
@@ -402,7 +503,7 @@ describe("getAuthorById", () => {
         expect(fetchAuthorTopicsById).not.toHaveBeenCalled();
         expect(fetchAuthorTopicSharesById).not.toHaveBeenCalled();
         expect(fetchAuthorCountsByYearById).not.toHaveBeenCalled();  
-        expect(fetchAuthorTop5Papers).not.toHaveBeenCalled();
+        expect(fetchAuthorPapers).not.toHaveBeenCalled();
     });
 
     // ------------ DATABASE ERRORS --------------
@@ -420,6 +521,143 @@ describe("getAuthorById", () => {
         expect(fetchAuthorTopicsById).not.toHaveBeenCalled();
         expect(fetchAuthorTopicSharesById).not.toHaveBeenCalled();
         expect(fetchAuthorCountsByYearById).not.toHaveBeenCalled();
-        expect(fetchAuthorTop5Papers).not.toHaveBeenCalled(); 
+        expect(fetchAuthorPapers).not.toHaveBeenCalled(); 
+    });
+});
+
+const defaultPaginationResults = [
+    ...mockResolvedPapers1,
+    ...mockResolvedPapers2
+];
+
+
+describe("getAuthorPapers", () => {
+    beforeEach(() => {
+        vi.resetAllMocks();
+    });
+
+    it("Returns the papers associated with an author using default pagination", async () => {
+        fetchAuthorById.mockResolvedValue(mockResolvedAuthor);
+        fetchAuthorPapers.mockResolvedValue(defaultPaginationResults);
+
+        const expectedOutput = defaultPaginationResults.map(paper => ({
+            id: paper.openalex_id,
+            internalId: paper.id,
+            title: paper.title,
+            displayName: paper.display_name,
+            abstract: paper.abstract,
+            publicationYear: paper.publication_year,
+            citedByCount: paper.cited_by_count,
+            fwci: Number(paper.fwci),
+            primarySource: paper.primary_source_display_name,
+            primaryTopic: paper.primary_topic_display_name,
+            isOpenAccess: paper.is_open_access,
+            openAccessStatus: paper.open_access_status,
+            authorCount: Number(paper.author_count),
+            authorsPreview: paper.authors_preview,
+        }));
+
+        const results = await getAuthorPapers("A5107860229", {page: null, limit: null});
+
+        expect(fetchAuthorPapers).toHaveBeenCalledWith("50703", 10, 0);
+        expect(fetchAuthorPapers).toHaveBeenCalledTimes(1);
+        expect(results).toEqual(expectedOutput);
+    });
+
+    it("Returns the papers associated with an author using custom pagination", async () => {
+        fetchAuthorById.mockResolvedValue(mockResolvedAuthor);
+        fetchAuthorPapers.mockResolvedValue(mockResolvedPapers2);
+
+        const expectedOutput = mockResolvedPapers2.map(paper => ({
+            id: paper.openalex_id,
+            internalId: paper.id,
+            title: paper.title,
+            displayName: paper.display_name,
+            abstract: paper.abstract,
+            publicationYear: paper.publication_year,
+            citedByCount: paper.cited_by_count,
+            fwci: Number(paper.fwci),
+            primarySource: paper.primary_source_display_name,
+            primaryTopic: paper.primary_topic_display_name,
+            isOpenAccess: paper.is_open_access,
+            openAccessStatus: paper.open_access_status,
+            authorCount: Number(paper.author_count),
+            authorsPreview: paper.authors_preview,
+        }));
+
+        const results = await getAuthorPapers("A5107860229", {page: 2, limit: 5});
+
+        expect(fetchAuthorPapers).toHaveBeenCalledWith("50703", 5, 5);
+        expect(fetchAuthorPapers).toHaveBeenCalledTimes(1);
+        expect(results).toEqual(expectedOutput);
+    });
+
+    // ------------ USER ERRORS --------------
+
+    it("Throws a 400 AppError when input id is not a string", async () => {
+        // Assuming default pagination for simplicity
+        await expect(getAuthorPapers(5107, {page: null, limit: null}))
+        .rejects
+        .toThrow("'author id' must be a string");
+
+        expect(fetchAuthorPapers).not.toHaveBeenCalled();
+    });
+
+    it("Throws a 400 AppError when input id is not a string", async () => {
+        // Assuming default pagination for simplicity
+        await expect(getAuthorPapers("5107", {page: null, limit: null}))
+        .rejects
+        .toThrow("Invalid author Id");
+
+        expect(fetchAuthorPapers).not.toHaveBeenCalled();
+    });
+
+    it("Throws a 400 AppError when page number is not a number", async () => {
+        // Assuming default pagination limit for simplicity
+        await expect(getAuthorPapers("A5107860229", {page: "first", limit: null}))
+        .rejects
+        .toThrow("'page' must be an integer");
+
+        expect(fetchAuthorPapers).not.toHaveBeenCalled();
+    });
+
+    it("Throws a 400 AppError when limit is not a number", async () => {
+        // Assuming default pagination page for simplicity
+        await expect(getAuthorPapers("A5107860229", {page: null, limit: "five"}))
+        .rejects
+        .toThrow("'limit' must be an integer");
+
+        expect(fetchAuthorPapers).not.toHaveBeenCalled();
+    });
+
+    it("Throws a 404 AppError when author doesn't exist", async () => {
+        fetchAuthorById.mockResolvedValue(null);
+
+        // Testing invalid author ID input
+        await expect(getAuthorPapers("A5107860", {page: null, limit: null}))
+        .rejects
+        .toThrow("Author not found");
+        
+        expect(fetchAuthorById).toHaveBeenCalledWith("A5107860");
+        expect(fetchAuthorById).toHaveBeenCalledTimes(1);
+  
+        expect(fetchAuthorPapers).not.toHaveBeenCalled();
+    });
+
+    // ------------ DATABASE ERRORS --------------
+
+    it("Propagates repository error", async () => {
+        fetchAuthorById.mockResolvedValue(mockResolvedAuthor);
+        fetchAuthorPapers.mockRejectedValue(new Error("Database query failed."));
+
+        await expect(getAuthorPapers("A5107860229", {page: null, limit: null}))
+        .rejects
+        .toThrow("Database query failed.");
+
+        expect(fetchAuthorById).toHaveBeenCalledWith("A5107860229");
+        expect(fetchAuthorById).toHaveBeenCalledTimes(1);
+
+        expect(fetchAuthorPapers).toHaveBeenCalledWith("50703", 10, 0);
+        expect(fetchAuthorPapers).toHaveBeenCalledTimes(1); 
     });
 });
