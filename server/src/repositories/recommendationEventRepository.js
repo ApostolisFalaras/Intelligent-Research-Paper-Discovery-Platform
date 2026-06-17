d9b5f05874543c20fd01cf2a9fe06b2b5e03ae3a
Apostolis Falaras <apostolisfalaras@gmail.com>
2026-06-17 16:08:01 +0300

    Implement the recommendationEvent service and repository for recording user actions and updating their individual interactions and global paper metrics

---
+++ server/src/repositories/recommendationEventRepository.js
@@ -0,0 +1,112 @@
+import pool from "./../config/db.js";
+
+// User-Paper Interactions
+
+// User views a paper -> update their individual paper interactions 
+export async function upsertPaperView(userId, paperId) {
+	const sqlQuery = `
+		INSERT INTO user_paper_interactions (
+			user_id, paper_id, view_count, is_saved, interest_score,
+			first_viewed_at, last_interaction_at
+		)
+		VALUES ($1, $2, 1, false, 1, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP)
+		ON CONFLICT (user_id, paper_id) DO UPDATE SET
+			view_count = user_paper_interactions.view_count + 1,
+			interest_score = user_paper_interactions.interest_score + 1,
+			last_interaction_at = CURRENT_TIMESTAMP;
+	`;
+
+	await pool.query(sqlQuery, [userId, paperId]);
+}
+
+// User saves a paper to a folder -> update their individual paper interactions
+export async function upsertPaperSave(userId, paperId) {
+	const sqlQuery = `
+		INSERT INTO user_paper_interactions (
+			user_id, paper_id, view_count, is_saved, interest_score,
+			first_viewed_at, last_interaction_at
+		)
+		VALUES ($1, $2, 1, true, 6, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP)
+		ON CONFLICT (user_id, paper_id) DO UPDATE SET
+			is_saved = true,
+			interest_score = user_paper_interactions.interest_score + 5,
+			last_interaction_at = CURRENT_TIMESTAMP;
+	`;
+
+	await pool.query(sqlQuery, [userId, paperId]);
+}
+
+// User un-saves a paper from a folder -> update their individual paper interactions
+export async function upsertPaperUnsave(userId, paperId) {
+	const sqlQuery = `
+		UPDATE user_paper_interactions
+		SET 
+			is_saved = false,
+			interest_score = view_count,
+			last_interaction_at = CURRENT_TIMESTAMP
+		WHERE user_id = $1 
+		  AND paper_id = $2;
+	`;
+
+	await pool.query(sqlQuery, [userId, paperId]);
+}
+
+
+// Global Paper Interactions
+
+// User view a paper -> Global paper view count incremented
+export async function incrementPaperViewCount(paperId) {
+	const sqlQuery = `
+		INSERT INTO paper_metrics (
+			paper_id, view_count, save_count, recommendation_click_count, popularity_score
+		) 
+		VALUES ($1, 1, 0, 0, 0)
+		ON CONFLICT (paper_id) DO UPDATE SET
+			view_count = paper_metrics.view_count + 1,
+			updated_at = CURRENT_TIMESTAMP;
+	`;
+
+	await pool.query(sqlQuery, [paperId]);
+}
+
+// User saves a paper -> Global paper save count incremented
+export async function incrementPaperSaveCount(paperId) {
+	const sqlQuery = `
+		INSERT INTO paper_metrics (
+			paper_id, view_count, save_count, recommendation_click_count, popularity_score
+		) 
+		VALUES ($1, 0, 1, 0, 0)
+		ON CONFLICT (paper_id) DO UPDATE SET
+			save_count = paper_metrics.save_count + 1,
+			updated_at = CURRENT_TIMESTAMP;
+	`;
+
+	await pool.query(sqlQuery, [paperId]);
+}
+
+// User un-saves a paper -> Global paper save count decremented
+export async function decrementPaperSaveCount(paperId) {
+	const sqlQuery = `
+		UPDATE paper_metrics 
+		SET save_count = GREATEST(save_count - 1, 0),
+			updated_at = CURRENT_TIMESTAMP
+		WHERE paper_id = $1;
+	`;
+
+	await pool.query(sqlQuery, [paperId]);
+}
+
+// User clicks a paper as a recommendation -> Global paper recomendation click count incremented
+export async function incrementRecommendationClickCount(paperId) {
+	const sqlQuery = `
+		INSERT INTO paper_metrics (
+			paper_id, view_count, save_count, recommendation_click_count, popularity_score
+		) 
+		VALUES ($1, 0, 0, 1, 0)
+		ON CONFLICT (paper_id) DO UPDATE SET
+			recommendation_click_count = paper_metrics.recommendation_click_count + 1,
+			updated_at = CURRENT_TIMESTAMP;
+	`;
+
+	await pool.query(sqlQuery, [paperId]);
+}
---
+++ server/src/services/recommendationEventService.js
@@ -0,0 +1,46 @@
+import { 
+	upsertPaperView,
+	upsertPaperSave,
+	upsertPaperUnsave,
+	incrementPaperViewCount,
+	incrementPaperSaveCount,
+	decrementPaperSaveCount,
+	incrementRecommendationClickCount } from "../repositories/recommendationEventRepository.js";
+import { parseUserId, parseInteger } from "../utils/parseData.js";
+import { AppError } from "./../utils/AppError.js";
+
+
+function validateUserAndPaperIds(userId, paperId) {
+	// Validate user & paper ids
+	const parsedUserId = parseUserId(userId);
+    const parsedPaperId = parseInteger(paperId, "paper id");
+
+	return [parsedUserId, parsedPaperId];
+}
+
+// Records a user's paper view
+export async function recordPaperView(userId, paperId, isRecommendation) {
+	const [parsedUserId, parsedPaperId] = validateUserAndPaperIds(userId, paperId);
+
+	await upsertPaperView(parsedUserId, parsedPaperId);
+	await incrementPaperViewCount(parsedPaperId);
+
+	if (isRecommendation)
+		incrementRecommendationClickCount(parsedPaperId);
+} 
+
+// Records a user's paper save
+export async function recordPaperSave(userId, paperId) {
+	const [parsedUserId, parsedPaperId] = validateUserAndPaperIds(userId, paperId);
+
+	await upsertPaperSave(parsedUserId, parsedPaperId);
+	await incrementPaperSaveCount(parsedPaperId);
+}
+
+// Records a user's paper un-save
+export async function recordPaperUnsave(userId, paperId) {
+	const [parsedUserId, parsedPaperId] = validateUserAndPaperIds(userId, paperId);
+
+	await upsertPaperUnsave(parsedUserId, parsedPaperId);
+	await decrementPaperSaveCount(parsedPaperId);
+}
