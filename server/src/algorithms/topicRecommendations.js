4d8441a2952aa2962d26d6eee7e4defcc8a16259
Apostolis Falaras <apostolisfalaras@gmail.com>
2026-06-24 17:09:28 +0300

    Add heuristic topic scoring function for topic score used in user-paper recommendations, alongside general topic scoring function according to the overall user profile

--- server/src/algorithms/topicRecommendations.js
+++ server/src/algorithms/topicRecommendations.js
@@ -1,5 +1,30 @@
-import { minmaxNormalizeArray } from "../utils/vectorUtils";
+import { minmaxNormalizeArray } from "./../utils/vectorUtils.js";
+import { cosineSimilarity } from "./../utils/vectorUtils";
 
+// Calculates a single paper's topic score in terms of a user's preferences
+export function calculatePaperTopicScores(userProfile, paperFeatures) {
+	const topicScore = cosineSimilarity(userProfile.topic_preferences, paperFeatures.topic_vector);
+	const fieldScore = cosineSimilarity(userProfile.field_preferences, paperFeatures.field_vector);
+	const subfieldScore = cosineSimilarity(userProfile.subfield_preferences, paperFeatures.subfield_vector);
+
+	return 0.60 * topicScore + 0.30 * fieldScore + 0.10 * subfieldScore;
+}
+
+// Calculates the topic score for a set of candidate user-paper pairs
+// It filters out negative topic scores,
+// and sorts the topic score records in descending order of score
+export function generatePaperTopicScores(userProfile, candidatePapers) {
+	return candidatePapers
+		.map(paper => ({
+			paperId: paper.paper_id,
+			topicScore: calculatePaperTopicScores(userProfile, paper)
+		}))
+		.filter(rec => rec.topicScore > 0)
+		.sort((a,b) => b.topicScore - a.topicScore);
+}
+
+
+// It's used for general topic recommendations to a user
 export function generateTopicRecommendations(userProfile, topicRows = [], limit = 20) {
 	const topicPreferences = userProfile.topic_preferences ?? {};
 
@@ -27,3 +52,6 @@ export function generateTopicRecommendations(userProfile, topicRows = [], limit 
 		.sort((a, b) => b.topicScore - a.topicScore)
 		.slice(0,limit);
 }
+
+
+
