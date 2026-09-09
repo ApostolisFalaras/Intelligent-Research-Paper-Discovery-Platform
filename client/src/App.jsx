ff3b8d3b3502ae44fb6dc55a0d28f6ff10dc9db0
Apostolis Falaras <apostolisfalaras@gmail.com>
2026-09-09 22:18:04 +0300

    Complete available routes for the BrowserRouter

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,17 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Toaster } from "sonner";
 import AppLayout from "./components/layout/AppLayout.jsx";
 import HomePage from "./HomePage.jsx";
-//import { SearchPage } from "./pages/SearchPage.jsx";
-//import { PaperPage } from "./pages/PaperPage.jsx";
-//import { AuthorPage } from "./pages/AuthorPage.jsx";
-//import { ProfilePage } from "./pages/ProfilePage.jsx";
-//import { ProjectsPage } from "./pages/ProjectsPage.jsx";
-//import { RecommendationsPage } from "./pages/RecommendationsPage.jsx";
+import AuthPage from "./AuthPage.jsx";
+import SearchPage from "./SearchPage.jsx";
+import PaperPage from "./PaperPage.jsx";
+import ExplorePage from "./ExplorePage.jsx";
+import ExploreTopicPage from "./ExploreTopicPage.jsx";
+import LibraryPage from "./LibraryPage.jsx";
+import ProfilePage from "./ProfilePage.jsx";
+import AccountSettingsPage from "./AccountSettingsPage.jsx";
+import RecommendationsPage from "./RecommendationsPage.jsx";
+import AuthorPage from "./AuthorPage.jsx";
 
 function App() {
   
@@ -16,14 +21,24 @@ function App() {
         {/* AppLayout is the base layout of all pages */}
         <Route element={<AppLayout />} >
           <Route path="/" element={<HomePage />} />
-          {/*<Route path="/search" element={<SearchPage />} />
-          <Route path="/paper/:id" element={<PaperPage />} />
+          <Route path="/auth" element={<AuthPage />} />
+          <Route path="/search" element={<SearchPage />} />
+          <Route path="/papers/:id" element={<PaperPage />} />
+          <Route path="/explore" element={<ExplorePage />} />
+          <Route path="/explore/topic/:id" element={<ExploreTopicPage />} />
+          <Route path="/my-library" element={<LibraryPage />} />
+          <Route path="/my-profile" element={<ProfilePage />} />
+          <Route path="/account-settings" element={<AccountSettingsPage />} />
+          <Route path="/recommendations" element={<RecommendationsPage />} />
           <Route path="/authors/:id" element={<AuthorPage />} />
-          <Route path="/me/profile" element={<ProfilePage />} />
-          <Route path="/me/folders" element={<ProjectsPage />} />
-          <Route path="/recommendations" element={< RecommendationsPage />} />*/}
         </Route>
       </Routes>
+
+      <Toaster 
+        position="bottom-right"
+        richColors
+        closeButton
+      />
     </BrowserRouter>
   );
 }
