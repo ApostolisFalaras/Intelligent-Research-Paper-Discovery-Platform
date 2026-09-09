db8fedbb209963512e8090d7bb6fe7dd92a524d3
Apostolis Falaras <apostolisfalaras@gmail.com>
2026-09-09 22:00:09 +0300

    Remove unused GET /api/topics/:id and GET /api/topics/:id/papers routes

--- server/src/controllers/topicController.js
+++ server/src/controllers/topicController.js
@@ -2,34 +2,6 @@ import { getTopicById, getTopicPapers, getAllTopics } from "./../services/topicS
 import { AppError } from "./../utils/AppError.js";
 
 
-// Retrieves a topic and its information
-export async function getTopicByIdController(req, res, next) {
-	try {
-		const topic = await getTopicById(req.params.id);
-
-		res.status(200).json({
-			status: "success",
-			data: topic
-		});
-	} catch(error) {
-		next(error);
-	}
-}
-
-// Retrieves papars associated with a particular topic
-export async function getTopicPapersController(req, res, next) {
-	try {
-		const papers = await getTopicPapers(req.params.id, req.query);
-
-		res.status(200).json({
-			status: "success",
-			data: papers
-		});
-	} catch(error) {
-		next(error);
-	}
-}
-
 // The app fetches all topics for the "Topic" dropdown search filter
 export async function getAllTopicsController(req, res, next) {
 	try {
--- server/src/routes/topicRoutes.js
+++ server/src/routes/topicRoutes.js
@@ -8,10 +8,4 @@ const router = express.Router();
 // The app fetches all topics for the "Topic" dropdown search filter
 router.get("/all", getAllTopicsController);
 
-// User view a particular topic's page
-router.get("/:id", getTopicByIdController);
-
-// User views papers related to the current topic
-router.get("/:id/papers", getTopicPapersController);
-
 export default router;
